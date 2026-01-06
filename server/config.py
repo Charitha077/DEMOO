b130442146b270eb2c24be89c6516150a9a81d98
Charitha Bodige <167196101+Charitha077@users.noreply.github.com>
2026-01-06 17:47:40 +0530

    Clear sensitive data in config.py
    Updated MONGO_URI and JWT_SECRET to empty strings.

--- server/config.py
+++ server/config.py
@@ -1,8 +1,8 @@
 # config.py
 class Config:
     # Switched database name from faceAuthDB to FaceSure
-    MONGO_URI = "mongodb+srv://vamshivardhan987_db_user:yHa8P9nAfn5dMyss@cluster0.l4bjzcp.mongodb.net/FaceSure?retryWrites=true&w=majority&appName=Cluster0"
-    JWT_SECRET = "SUPER_SECRET_KEY"
+    MONGO_URI = ""
+    JWT_SECRET = ""
 
     SUPERADMINS = [
         {
